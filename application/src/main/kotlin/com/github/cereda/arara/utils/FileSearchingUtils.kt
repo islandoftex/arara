d7c9003ae7ef3d2ffbf5bb6e7acbd8575347d1d2
Ben Frank <ben.frank@mail.de>
2019-10-13 20:06:14 +0200

    Fix severe error in the file searching logic

--- application/src/main/kotlin/com/github/cereda/arara/utils/FileSearchingUtils.kt
+++ application/src/main/kotlin/com/github/cereda/arara/utils/FileSearchingUtils.kt
@@ -36,7 +36,6 @@ package com.github.cereda.arara.utils
 import java.io.File
 import java.io.FileFilter
 import java.nio.file.FileSystems
-import java.nio.file.Files
 
 /**
  * Implements file searching utilitary methods.
@@ -55,26 +54,24 @@ object FileSearchingUtils {
      * @return A list of files.
      */
     fun listFilesByExtensions(directory: File,
-                              extensions: List<String>, recursive: Boolean): List<File> {
-        return try {
-            // return the result of the
-            // provided search
-            if (recursive)
-                Files.newDirectoryStream(directory.toPath())
-                        .map { it.toFile() }
-                        .filter { extensions.contains(it.extension) }
-                        .toList()
-            else
-                directory.listFiles(
-                        FileFilter { extensions.contains(it.extension) })!!
-                        .toList()
-        } catch (_: Exception) {
-            // if something bad happens,
-            // gracefully fallback to
-            // an empty file list
-            listOf()
-        }
-
+                              extensions: List<String>, recursive: Boolean):
+            List<File> = try {
+        // return the result of the
+        // provided search
+        if (recursive)
+            directory.walkTopDown().asSequence()
+                    .filter { !it.isDirectory }
+                    .filter { extensions.contains(it.extension) }
+                    .toList()
+        else
+            directory.listFiles(
+                    FileFilter { extensions.contains(it.extension) })!!
+                    .toList()
+    } catch (_: Exception) {
+        // if something bad happens,
+        // gracefully fallback to
+        // an empty file list
+        listOf()
     }
 
     /**
@@ -86,27 +83,28 @@ object FileSearchingUtils {
      * @return A list of files.
      */
     fun listFilesByPatterns(directory: File,
-                            patterns: List<String>, recursive: Boolean): List<File> {
-        return try {
-            // return the result of the provided
-            // search, with the wildcard filter
-            // and a potential recursive search
-            val pathMatcher = patterns.map {
-                FileSystems.getDefault().getPathMatcher("glob:$it")
-            }
-            if (recursive)
-                Files.newDirectoryStream(directory.toPath()) { path ->
-                    pathMatcher.any { it.matches(path) }
-                }.map { it.toFile() }.toList()
-            else
-                directory.listFiles { file: File ->
-                    pathMatcher.any { it.matches(file.toPath()) }
-                }!!.toList()
-        } catch (_: Exception) {
-            // if something bad happens,
-            // gracefully fallback to
-            // an empty file list
-            listOf()
+                            patterns: List<String>, recursive: Boolean):
+            List<File> = try {
+        // return the result of the provided
+        // search, with the wildcard filter
+        // and a potential recursive search
+        val pathMatcher = patterns.map {
+            FileSystems.getDefault().getPathMatcher("glob:$it")
         }
+        if (recursive)
+            directory.walkTopDown().asSequence()
+                    .filter { !it.isDirectory }
+                    .filter { file ->
+                        pathMatcher.any { it.matches(file.toPath().fileName) }
+                    }.toList()
+        else
+            directory.listFiles { file: File ->
+                pathMatcher.any { it.matches(file.toPath().fileName) }
+            }!!.toList()
+    } catch (_: Exception) {
+        // if something bad happens,
+        // gracefully fallback to
+        // an empty file list
+        listOf()
     }
 }
---
+++ application/src/test/kotlin/com/github/cereda/arara/utils/FileSearchingUtilsTest.kt
@@ -0,0 +1,39 @@
+package com.github.cereda.arara.utils
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.ShouldSpec
+import java.nio.file.Files
+import java.nio.file.Path
+
+class FileSearchingUtilsTest : ShouldSpec({
+    fun prepareFileSystem(): Path {
+        val tempDir = Files.createTempDirectory(System.nanoTime().toString())
+        tempDir.resolve("quack/quack").toFile().mkdirs()
+        listOf("quack", "quack/quack", "quack/quack/quack").forEach {
+            tempDir.resolve("$it.tex").toFile().writeText(" ")
+            tempDir.resolve("$it.txt").toFile().writeText(" ")
+        }
+        return tempDir
+    }
+
+    should("find file by extension") {
+        val tempDir = prepareFileSystem()
+        FileSearchingUtils.listFilesByExtensions(tempDir.toFile(),
+                listOf("tex"), false).toSet() shouldBe
+                setOf(tempDir.resolve("quack.tex").toFile())
+        FileSearchingUtils.listFilesByExtensions(tempDir.toFile(),
+                listOf("tex"), true).toSet() shouldBe
+                listOf("quack", "quack/quack", "quack/quack/quack")
+                        .map { tempDir.resolve("$it.tex").toFile() }.toSet()
+    }
+    should("find file by pattern") {
+        val tempDir = prepareFileSystem()
+        FileSearchingUtils.listFilesByPatterns(tempDir.toFile(),
+                listOf("*q*.txt"), false).toSet() shouldBe
+                setOf(tempDir.resolve("quack.txt").toFile())
+        FileSearchingUtils.listFilesByPatterns(tempDir.toFile(),
+                listOf("q*.txt"), true).toSet() shouldBe
+                listOf("quack", "quack/quack", "quack/quack/quack")
+                        .map { tempDir.resolve("$it.txt").toFile() }.toSet()
+    }
+})
