f844fa685b4b7124d21ca8e0cd219f94f013f447
Ben Frank <ben.frank@mail.de>
2019-12-08 18:39:58 +0100

    Bump detekt version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     kotlin("jvm") version "1.3.61" apply false                        // Apache 2.0
     id("com.github.johnrengelman.shadow") version "5.2.0" apply false // Apache 2.0
     id("org.jetbrains.dokka") version "0.10.0" apply false            // Apache 2.0
-    id("io.gitlab.arturbosch.detekt") version "1.2.0"                 // Apache 2.0
+    id("io.gitlab.arturbosch.detekt") version "1.2.2"                 // Apache 2.0
     id("com.github.ben-manes.versions") version "0.27.0"              // Apache 2.0
 }
 
