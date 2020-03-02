311962ac111cf8973b5541ce90a53e8266d5523f
Ben Frank <ben.frank@mail.de>
2020-03-02 22:33:00 +0100

    Fix broken CRC test

--- application/src/test/kotlin/org/islandoftex/arara/filehandling/FileHandlingUtilsTest.kt
+++ application/src/test/kotlin/org/islandoftex/arara/filehandling/FileHandlingUtilsTest.kt
@@ -17,7 +17,7 @@ class FileHandlingUtilsTest : ShouldSpec({
         }
     }
     should("generate correct CRC sums") {
-        FileHandlingUtils.calculateHash(File("../LICENSE")) shouldBe "fa68960d"
+        FileHandlingUtils.calculateHash(File("../LICENSE")) shouldBe "2396b4e2"
         FileHandlingUtils.calculateHash(File("../CODE_OF_CONDUCT.md")) shouldBe "536c426f"
     }
 
