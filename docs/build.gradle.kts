d21bec33ada1f24622b3ac8492505e20cf1bdc66
Ben Frank <ben.frank@mail.de>
2020-03-13 11:36:03 +0100

    Fix Gradle task to build the documentation

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -1,13 +1,20 @@
 dependencies {
-  project(":application")
+    project(":application")
 }
 
 tasks.create<JavaExec>("buildManual") {
-  group = "documentation"
-  description = "Compile the manual's TeX file to PDF."
-  
-  classpath = files(project(":application").tasks.findByPath("shadowJar"))
-  args = listOf("-l", "-v", "arara-manual.tex")
-  inputs.files("arara-manual.tex")
-  outputs.files("arara-manual.pdf")
+    group = "documentation"
+    description = "Compile the manual's TeX file to PDF."
+
+    classpath = files(project(":application").tasks.findByPath("shadowJar"))
+    args = listOf("-l", "-v", "arara-manual.tex")
+    inputs.dir("chapters")
+    inputs.dir("figures")
+    inputs.dir("logos")
+    inputs.file("rules/manual.yaml")
+    inputs.file("arara.sty")
+    inputs.file("arararc.yaml")
+    inputs.file("arara-manual.tex")
+    outputs.files("arara-manual.pdf")
+    outputs.upToDateWhen { false }
 }
