ac736d8ef3589acdb01ec6b2117748574afee335
Ben Frank <ben.frank@mail.de>
2020-04-14 11:26:02 +0200

    Add file type resolution in DSL

--- kotlin-dsl/src/main/kotlin/org/islandoftex/arara/dsl/language/DSLProjectFile.kt
+++ kotlin-dsl/src/main/kotlin/org/islandoftex/arara/dsl/language/DSLProjectFile.kt
@@ -3,6 +3,7 @@ package org.islandoftex.arara.dsl.language
 
 import java.nio.file.Path
 import org.islandoftex.arara.api.files.FileType
+import org.islandoftex.arara.core.configuration.ConfigurationUtils
 import org.islandoftex.arara.core.files.ProjectFile
 import org.islandoftex.arara.core.files.UNKNOWN_TYPE
 
@@ -10,10 +11,12 @@ class DSLProjectFile(private val path: Path) {
     var directives: List<String>? = null
     var priority: Int = ProjectFile.DEFAULT_PRIORITY
 
-    fun toProjectFile(): org.islandoftex.arara.api.files.ProjectFile =
-            org.islandoftex.arara.dsl.files.ProjectFile(
-                    path, FileType.UNKNOWN_TYPE, priority
-            ).apply {
-                directives = this@DSLProjectFile.directives
-            }
+    fun toProjectFile(): org.islandoftex.arara.api.files.ProjectFile {
+        // TODO: use file types from execution
+        val fileType = ConfigurationUtils.defaultFileTypes
+                .find { it.extension == path.toString().substringAfterLast('.') }
+                ?: FileType.UNKNOWN_TYPE
+        return org.islandoftex.arara.dsl.files.ProjectFile(path, fileType, priority)
+                .apply { directives = this@DSLProjectFile.directives }
+    }
 }
