877fbf03da3db8c30fed9dfaad5c08d2fa1ae104
Ben Frank <ben.frank@mail.de>
2020-09-17 21:54:53 +0200

    Add tests for command execution

--- core/src/test/kotlin/org/islandoftex/arara/core/session/EnvironmentTest.kt
+++ core/src/test/kotlin/org/islandoftex/arara/core/session/EnvironmentTest.kt
@@ -4,6 +4,9 @@ package org.islandoftex.arara.core.session
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import java.util.concurrent.TimeoutException
+import kotlin.time.milliseconds
 
 class EnvironmentTest : ShouldSpec({
     should("return null on non-existent system variable") {
@@ -18,5 +21,26 @@ class EnvironmentTest : ShouldSpec({
     }
     should("have a non-fallback value for existent variable") {
         listOf("/", "\\") shouldContain Environment.getSystemProperty("file.separator", "fallback")
+    }
+
+    if (Environment.getSystemPropertyOrNull("os.name")?.toLowerCase()
+                    ?.startsWith("windows") != true) {
+        // if we are not on Windows execute tests with execution
+        should("run successfully") {
+            val (exit, output) = Environment.executeSystemCommand(Command(listOf("true")))
+            exit shouldBe 0
+            output shouldBe ""
+        }
+        should("error with exit code 1") {
+            val (exit, output) = Environment.executeSystemCommand(Command(listOf("false")))
+            exit shouldBe 1
+            output shouldBe ""
+        }
+        should("error with timeout exception") {
+            val (exit, output) = Environment.executeSystemCommand(
+                    Command(listOf("sleep", "1s")), true, 500.milliseconds)
+            exit shouldBe Environment.errorExitStatus
+            output shouldContain TimeoutException::class.java.name
+        }
     }
 })
