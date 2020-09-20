03e5f206bbd02fc09e48cf8a5eb6100c242bf62d
Ben Frank <ben.frank@mail.de>
2020-09-20 12:13:08 +0200

    Add test for logging

---
+++ cli/src/test/kotlin/org/islandoftex/arara/cli/utils/LoggingUtilsTest.kt
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause
+package org.islandoftex.arara.cli.utils
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import org.apache.logging.log4j.ThreadContext
+import org.islandoftex.arara.core.configuration.LoggingOptions
+
+class LoggingUtilsTest : ShouldSpec({
+    should("properly set log file path") {
+        val defaultEnable = LoggingOptions(true)
+        ThreadContext.get("araraLogFile") shouldBe null
+        LoggingUtils.setupLogging(defaultEnable)
+        ThreadContext.get("araraLogFile") shouldBe defaultEnable.logFile.toString()
+    }
+})
