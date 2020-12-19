ad61dd906a5bdb6348dc87ac5d4125f301721ab8
Ben Frank <ben.frank@mail.de>
2020-12-19 10:14:40 +0100

    Fix broken equals method for MPPLocale

--- api/src/jvmMain/kotlin/org/islandoftex/arara/api/localization/MPPLocale.kt
+++ api/src/jvmMain/kotlin/org/islandoftex/arara/api/localization/MPPLocale.kt
@@ -10,10 +10,19 @@ public actual class MPPLocale actual constructor(tag: String) {
 
     public fun toJVMLocale(): Locale = locale
 
-    override fun equals(other: Any?): Boolean =
-            locale == other
     override fun hashCode(): Int =
             locale.hashCode()
     override fun toString(): String =
             locale.toString()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MPPLocale
+
+        if (locale != other.locale) return false
+
+        return true
+    }
 }
