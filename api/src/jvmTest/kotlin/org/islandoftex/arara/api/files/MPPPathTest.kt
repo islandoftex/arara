440042ab74518ff6313167087c0ab21d64376294
Ben Frank <ben.frank@mail.de>
2020-12-19 16:10:13 +0100

    Make MPPPathTest Java-agnostic

--- api/src/jvmTest/kotlin/org/islandoftex/arara/api/files/MPPPathTest.kt
+++ api/src/jvmTest/kotlin/org/islandoftex/arara/api/files/MPPPathTest.kt
@@ -6,8 +6,6 @@ import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.engine.spec.tempfile
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
-import java.nio.file.Paths
-import kotlin.io.path.createTempDirectory
 import org.islandoftex.arara.api.AraraIOException
 
 class MPPPathTest : ShouldSpec({
@@ -17,8 +15,7 @@ class MPPPathTest : ShouldSpec({
                     MPPPath("/tmp/").normalize().toString()
         }
         should("not exceed root") {
-            MPPPath("/tmp/../../..").normalize().toString() shouldBe
-                    Paths.get("/..").toAbsolutePath().normalize().toString()
+            MPPPath("/tmp/../../..").normalize().toString() shouldBe "/"
         }
     }
     context("parenting") {
@@ -46,13 +43,13 @@ class MPPPathTest : ShouldSpec({
             tmp.readText() shouldBe "quack"
         }
         should("fail reading from directory") {
-            val tmp = createTempDirectory()
+            val tmp = tempfile().mkdir().toString()
             shouldThrow<AraraIOException> {
                 MPPPath(tmp).readText()
             }.message shouldContain "read text from file"
         }
         should("fail writing to directory") {
-            val tmp = createTempDirectory()
+            val tmp = tempfile().mkdir().toString()
             shouldThrow<AraraIOException> {
                 MPPPath(tmp).writeText("quack")
             }.message shouldContain "write text to file"
