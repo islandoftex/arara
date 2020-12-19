5464d6398ee7606ab394a3ca152e4b50a89c17a8
Ben Frank <ben.frank@mail.de>
2020-12-19 09:35:04 +0100

    Improve MPPLocale documentation

--- api/src/commonMain/kotlin/org/islandoftex/arara/api/localization/MPPLocale.kt
+++ api/src/commonMain/kotlin/org/islandoftex/arara/api/localization/MPPLocale.kt
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: BSD-3-Clause
 package org.islandoftex.arara.api.localization
 
+/**
+ * Represent a locale independent of the platform in use.
+ *
+ * Constructed from a locale tag, it is only used to hold th state of the
+ * locale and provide a display language.
+ */
 public expect class MPPLocale(tag: String) {
+    /**
+     * Display language of the language in use. If not possible to provide,
+     * falls back to language tag.
+     */
     public val displayLanguage: String
 }
