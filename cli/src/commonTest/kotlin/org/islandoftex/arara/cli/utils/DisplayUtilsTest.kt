4806c8e0599094c93fa59aa3070fce2a98da1953
Ben Frank <ben.frank@mail.de>
2021-01-03 18:03:30 +0100

    Move DisplayUtils test to common

--- cli/src/commonTest/kotlin/org/islandoftex/arara/cli/utils/DisplayUtilsTest.kt
+++ cli/src/commonTest/kotlin/org/islandoftex/arara/cli/utils/DisplayUtilsTest.kt
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: BSD-3-Clause
 package org.islandoftex.arara.cli.utils
 
-import io.kotest.core.spec.style.ShouldSpec
-import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+import kotlin.test.assertEquals
 import org.islandoftex.arara.api.localization.MPPLocale
 import org.islandoftex.arara.core.configuration.UserInterfaceOptions
 import org.islandoftex.arara.core.session.Session
 
-class DisplayUtilsTest : ShouldSpec({
-    should("format bytes correctly") {
+class DisplayUtilsTest {
+    @Test
+    fun shouldFormatBytesCorrectly() {
         Session.userInterfaceOptions = UserInterfaceOptions(MPPLocale("en"))
         mapOf(800 to "800 B",
                 1000 to "1.0 kB",
                 1024 to "1.0 kB",
                 1000000 to "1.0 MB").forEach { (key, value) ->
-            DisplayUtils.byteSizeToString(key.toLong()) shouldBe value
+            assertEquals(DisplayUtils.byteSizeToString(key.toLong()), value)
         }
     }
-})
+}
