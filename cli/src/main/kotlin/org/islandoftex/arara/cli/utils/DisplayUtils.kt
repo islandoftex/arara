acae2b265857a3d5e46407144ac0382bf27cb204
Ben Frank <ben.frank@mail.de>
2021-04-09 11:32:52 +0200

    Fix #70

--- cli/src/main/kotlin/org/islandoftex/arara/cli/utils/DisplayUtils.kt
+++ cli/src/main/kotlin/org/islandoftex/arara/cli/utils/DisplayUtils.kt
@@ -29,6 +29,16 @@ object DisplayUtils {
     private val logger = LoggerFactory.getLogger(DisplayUtils::class.java)
 
     /**
+     * The application logo for the terminal.
+     */
+    val logoString: String = """
+              __ _ _ __ __ _ _ __ __ _
+             / _` | '__/ _` | '__/ _` |
+            | (_| | | | (_| | | | (_| |
+             \__,_|_|  \__,_|_|  \__,_|
+    """.trimIndent()
+
+    /**
      * The file name of the configuration file in use. It is only needed for
      * logging purposes.
      */
@@ -359,19 +369,6 @@ object DisplayUtils {
                 )
         logger.info(text)
         printWrapped(text)
-    }
-
-    /**
-     * Displays the application logo in the terminal.
-     */
-    fun printLogo() {
-        println("""
-              __ _ _ __ __ _ _ __ __ _
-             / _` | '__/ _` | '__/ _` |
-            | (_| | | | (_| | | | (_| |
-             \__,_|_|  \__,_|_|  \__,_|
-        """.trimIndent())
-        println()
     }
 
     /**
