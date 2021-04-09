4660a2dfd88dadbb183e9f0344f8b532e3b5b8ca
Ben Frank <ben.frank@mail.de>
2021-04-09 12:57:03 +0200

    Fix directive fetching from Kotlin DSL

--- kotlin-dsl/src/jvmMain/kotlin/org/islandoftex/arara/dsl/files/ProjectFile.kt
+++ kotlin-dsl/src/jvmMain/kotlin/org/islandoftex/arara/dsl/files/ProjectFile.kt
@@ -6,6 +6,7 @@ import org.islandoftex.arara.api.files.FileType
 import org.islandoftex.arara.api.files.toMPPPath
 import org.islandoftex.arara.api.rules.Directive
 import org.islandoftex.arara.core.files.ProjectFile
+import org.islandoftex.arara.core.rules.Directives
 
 /**
  * Implementation of a project file with the option to specify directives
@@ -20,7 +21,7 @@ internal class ProjectFile(
      * The directives to be used instead of fetching them from file. Used
      * whenever non-null.
      */
-    internal var directives: List<String>? = null
+    var directives: List<String>? = null
 
     /**
      * Get the file's directives. This does not put them into any context,
@@ -29,7 +30,9 @@ internal class ProjectFile(
      * @return The list of directives in the file, in order.
      */
     override fun fetchDirectives(parseOnlyHeader: Boolean): List<Directive> {
-        // TODO: proper implementation
-        return listOf()
+        // TODO: exception handling
+        return directives?.let {
+            Directives.extractDirectives(it, true, fileType)
+        } ?: super.fetchDirectives(parseOnlyHeader)
     }
 }
