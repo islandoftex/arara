0d5a304f601c45c2503d9cffdfa8676c8446460d
Ben Frank <ben.frank@mail.de>
2021-06-25 22:54:21 +0200

    Bump dependencies

--- buildSrc/src/main/kotlin/org/islandoftex/arara/build/Versions.kt
+++ buildSrc/src/main/kotlin/org/islandoftex/arara/build/Versions.kt
@@ -3,13 +3,13 @@ package org.islandoftex.arara.build
 
 object Versions {
     // plugin dependencies
-    const val detekt = "1.16.0"
-    const val dokka = "1.4.30"
+    const val detekt = "1.17.1"
+    const val dokka = "1.4.32"
     const val kotlin = "1.4.32"
-    const val shadow = "6.1.0"
-    const val spotless = "5.11.1"
-    const val spotlessChangelog = "2.0.1"
-    const val versionsPlugin = "0.38.0"
+    const val shadow = "7.0.0"
+    const val spotless = "5.14.0"
+    const val spotlessChangelog = "2.2.0"
+    const val versionsPlugin = "0.39.0"
 
     // non-plugin dependencies
     const val clikt = "3.1.0"
@@ -17,7 +17,7 @@ object Versions {
     const val kaml = "0.29.0"
     const val kotest = "4.4.3"
     const val kotlinxSerialization = "1.0.1"
-    const val jackson = "2.12.2"
+    const val jackson = "2.12.3"
     const val log4j = "2.14.1"
     const val mvel = "2.4.12.Final"
     const val slf4j = "1.7.30"
