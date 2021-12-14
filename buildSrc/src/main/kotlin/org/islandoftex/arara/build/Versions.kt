36bb28309024aac3fe29547757dd40baba1c2a3d
Ben Frank <ben.frank@mail.de>
2021-12-14 09:08:01 +0100

    Fix #77

--- buildSrc/src/main/kotlin/org/islandoftex/arara/build/Versions.kt
+++ buildSrc/src/main/kotlin/org/islandoftex/arara/build/Versions.kt
@@ -3,23 +3,23 @@ package org.islandoftex.arara.build
 
 object Versions {
     // plugin dependencies
-    const val detekt = "1.17.1"
-    const val dokka = "1.4.32"
-    const val kotlin = "1.5.20"
-    const val shadow = "7.0.0"
+    const val detekt = "1.19.0"
+    const val dokka = "1.6.0"
+    const val kotlin = "1.6.0"
+    const val shadow = "7.1.0"
     const val spotless = "5.14.0"
-    const val spotlessChangelog = "2.2.0"
+    const val spotlessChangelog = "2.3.2"
     const val versionsPlugin = "0.39.0"
 
     // non-plugin dependencies
-    const val clikt = "3.2.0"
-    const val jna = "5.8.0"
-    const val kaml = "0.34.0"
+    const val clikt = "3.3.0"
+    const val jna = "5.10.0"
+    const val kaml = "0.37.0"
     const val kotest = "4.6.0"
-    const val kotlinxSerialization = "1.2.1"
-    const val jackson = "2.12.3"
-    const val log4j = "2.14.1"
+    const val kotlinxSerialization = "1.3.1"
+    const val jackson = "2.13.0"
+    const val log4j = "2.16.0"
     const val mvel = "2.4.12.Final"
-    const val slf4j = "1.7.30"
+    const val slf4j = "1.7.32"
     const val ztExec = "1.12"
 }
