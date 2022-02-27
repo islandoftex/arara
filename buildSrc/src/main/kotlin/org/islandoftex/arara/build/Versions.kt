4ae555361e24769973ff6367db853c70980cc830
Paulo Roberto Massa Cereda <cereda.paulo@gmail.com>
2022-02-27 06:38:01 -0300

    Updated dependencies

--- buildSrc/src/main/kotlin/org/islandoftex/arara/build/Versions.kt
+++ buildSrc/src/main/kotlin/org/islandoftex/arara/build/Versions.kt
@@ -4,22 +4,22 @@ package org.islandoftex.arara.build
 object Versions {
     // plugin dependencies
     const val detekt = "1.19.0"
-    const val dokka = "1.6.0"
+    const val dokka = "1.6.10"
     const val kotlin = "1.6.10"
-    const val shadow = "7.1.1"
-    const val spotless = "5.14.0"
-    const val spotlessChangelog = "2.3.2"
-    const val versionsPlugin = "0.39.0"
+    const val shadow = "7.1.2"
+    const val spotless = "6.3.0"
+    const val spotlessChangelog = "2.4.0"
+    const val versionsPlugin = "0.42.0"
 
     // non-plugin dependencies
-    const val clikt = "3.3.0"
+    const val clikt = "3.4.0"
     const val jna = "5.10.0"
-    const val kaml = "0.38.0"
-    const val kotest = "4.6.0"
-    const val kotlinxSerialization = "1.3.1"
+    const val kaml = "0.40.0"
+    const val kotest = "5.1.0"
+    const val kotlinxSerialization = "1.3.2"
     const val jackson = "2.13.1"
-    const val log4j = "2.17.0"
+    const val log4j = "2.17.2"
     const val mvel = "2.4.14.Final"
-    const val slf4j = "1.7.32"
+    const val slf4j = "1.7.36"
     const val ztExec = "1.12"
 }
