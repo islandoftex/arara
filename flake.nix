01302eda011e3db7bad8f0fbe190d634c58f8e4d
Ben Frank <ben.frank@mail.de>
2022-04-23 12:57:44 +0200

    Use only x86_64 in flake to allow running gradle

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
