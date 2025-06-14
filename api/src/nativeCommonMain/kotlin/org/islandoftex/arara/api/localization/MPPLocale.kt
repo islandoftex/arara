c35100c00418be7da6578a679bca57ceda269ade
Paulo Roberto Massa Cereda <cereda.paulo@gmail.com>
2025-06-14 13:02:33 -0300

    Removed old reference to native main

--- api/src/nativeCommonMain/kotlin/org/islandoftex/arara/api/localization/MPPLocale.kt
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-package org.islandoftex.arara.api.localization
-
-public actual class MPPLocale actual constructor(tag: String) {
-    // TODO: proper implementation
-    public actual val displayLanguage: String = tag
-    public actual val decimalSeparator: Char = '.'
-}
