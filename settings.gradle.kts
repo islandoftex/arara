da66cedbe6541001f1ddb3bd07211aef81402e4d
Paulo Roberto Massa Cereda <cereda.paulo@gmail.com>
2025-06-27 18:09:26 -0300

    Updated CI builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 // automatic toolchain resolution
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "arara"
