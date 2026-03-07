64b288c3d302f456ebde4644da0c779db9de36c4
Ben Frank <ben.frank@mail.de>
2026-03-07 16:20:07 +0100

    Update zola, juice theme, and build via flake

--- flake.nix
+++ flake.nix
@@ -11,47 +11,96 @@
     };
   };
 
-  outputs = { self, flake-utils, nixpkgs, gradle2nix }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      gradle2nix,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        defaultPackage = gradle2nix.builders.${system}.buildGradlePackage rec {
-          pname = "arara";
-          # This version corresponds to the major version of arara.
-          # If a more granular adjustment is needed, please raise an issue.
-          # see also https://gitlab.com/islandoftex/arara/-/issues/97
-          version = "7.0.0";
+        formatter = pkgs.nixfmt-tree;
 
-          src = ./.;
+        packages =
+          let
+            # This version corresponds to the major version of arara.
+            # If a more granular adjustment is needed, please raise an issue.
+            # see also https://gitlab.com/islandoftex/arara/-/issues/97
+            version = "7.0.0";
+          in
+          {
+            default = gradle2nix.builders.${system}.buildGradlePackage {
+              inherit version;
+              pname = "arara";
 
-          # generated using `gradle2nix` (no arguments required)
-          lockFile = ./gradle.lock;
+              src = ./.;
 
-          nativeBuildInputs = [ pkgs.makeWrapper ];
+              # generated using `gradle2nix` (no arguments required)
+              lockFile = ./gradle.lock;
 
-          gradleBuildFlags = [ "--no-daemon" "assembleDist" ];
+              nativeBuildInputs = [ pkgs.makeWrapper ];
 
-          installPhase = ''
-            mkdir -p $out/share/java/
+              gradleBuildFlags = [
+                "--no-daemon"
+                "assembleDist"
+              ];
 
-            install -Dm644 cli/build/libs/arara-cli-with-deps-*.jar $out/share/java/arara-${version}.jar
-            install -Dm644 -t $out/share/java/rules rules/*
+              installPhase = ''
+                mkdir -p $out/share/java/
 
-            makeWrapper ${pkgs.jre}/bin/java $out/bin/arara \
-              --add-flags "-jar $out/share/java/arara-${version}.jar"
-          '';
+                install -Dm644 cli/build/libs/arara-cli-with-deps-*.jar $out/share/java/arara-${version}.jar
+                install -Dm644 -t $out/share/java/rules rules/*
 
-          meta = with pkgs.lib; {
-            homepage = "https://gitlab.com/islandoftex/arara";
-            description = "arara is a TeX automation tool based on rules and directives. It gives you a way to enhance your TeX experience.";
-            license = licenses.bsd3;
+                makeWrapper ${pkgs.jre}/bin/java $out/bin/arara \
+                  --add-flags "-jar $out/share/java/arara-${version}.jar"
+              '';
+
+              meta = with pkgs.lib; {
+                homepage = "https://gitlab.com/islandoftex/arara";
+                description = "arara is a TeX automation tool based on rules and directives. It gives you a way to enhance your TeX experience.";
+                license = licenses.bsd3;
+              };
+            };
+            website =
+              let
+                juiceTheme = pkgs.fetchFromGitHub {
+                  owner = "huhu";
+                  repo = "juice";
+                  rev = "0dc909d59066a0c8cc9f7bcaa6f6853e7574452d";
+                  hash = "sha256-WtnU0CA4yf9Cad07uvIy+ma/nlrkNChUnWY38fvyZq8=";
+                };
+              in
+              pkgs.stdenvNoCC.mkDerivation {
+                inherit version;
+                pname = "arara-website";
+
+                nativeBuildInputs = [
+                  pkgs.zola
+                ];
+
+                src = ./website;
+
+                # TODO: set updated date based on git if date != updated for all posts
+                buildPhase = ''
+                  mkdir -p themes/juice
+                  cp -rf ${juiceTheme}/{theme.toml,sass,static,templates} themes/juice/
+                  zola build
+                '';
+
+                installPhase = ''
+                  mkdir -p $out/public
+                  cp -r public/. $out/public/
+                '';
+              };
           };
-        };
 
-        devShell = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.defaultPackage;
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system}.default;
           buildInputs = with pkgs; [
             gradle2nix.packages.${system}.gradle2nix
             corretto11
